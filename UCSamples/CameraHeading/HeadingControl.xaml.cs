39ace962e5dcc288aadeaf752d0fb52246fdc244
Uma Harano <uharano@esri.com>
2014-07-14 15:51:56 -0700

    Added copyright

--- UCSamples/CameraHeading/HeadingControl.xaml.cs
+++ UCSamples/CameraHeading/HeadingControl.xaml.cs
@@ -1,4 +1,18 @@
-﻿/// A compass like control for manipulating the camera heading.
+﻿//Copyright 2014 Esri
+ 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+ 
+//       http://www.apache.org/licenses/LICENSE-2.0
+ 
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/// A compass like control for manipulating the camera heading.
 /// Essentially we are using a templated slider control for this approach.
 ///
 /// inspired by http://alala666888.wordpress.com/2010/05/31/a-knob-slider-control/
@@ -66,6 +80,13 @@ namespace UCSamples.CameraHeading
                 CameraHeadingControl.Value = (CameraHeadingControl.Maximum - CameraHeadingControl.Minimum) * angle / (2 * Math.PI);
             }
         }
+        private void ResetHeading(object sender, MouseButtonEventArgs e) {
+            CameraHeadingControl.Value = 0;
+
+            BindingExpression binding = CameraHeadingControl.GetBindingExpression(Slider.ValueProperty);
+            binding.UpdateSource();
+        }
+
     }
 
 
